9791364a31f6d9524eff3870bb23114c04f3688e
melissawahnish <melissawahnish@gmail.com>
2015-01-03 15:42:22 -0500

    Twitter link
    woo hoo, finally figured out how to put locals into a twitter
    share

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+	def twitter_link(twitter_text)
+		link_to 'Twitter', "http://twitter.com/share?text=#{URI.escape(twitter_text)}", 
+		'class' => 'twitter-share-button'
+	end	
+
+	def twitter_text(noun, business)
+		"I just founded #{business.name} for #{noun.name}! Check it out! @derivativestartups #derivativestartups"
+	end	
 end
